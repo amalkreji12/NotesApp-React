fa8e7325fabc3a4a65a3211bd662aa2d8d2d3086
Amal K Reji <amalreji45.amal@gmail.com>
2024-12-06 15:24:29 +0530

    login page

--- frontend/notes/src/components/NavBar/NavBar.jsx
+++ frontend/notes/src/components/NavBar/NavBar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+      <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
+    </div>
   )
 }
 
--- frontend/notes/src/pages/Login/Login.jsx
+++ frontend/notes/src/pages/Login/Login.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import { Link } from "react-router-dom"
+import InputPassword from '../../components/Input/InputPassword'
+import { validateEmail } from '../../utils/helper';
 function Login() {
+
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [error,setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)) {
+      setError("Please enter a valid email");
+      return;
+    }
+
+    if(!password){
+      setError("Please enter a password");
+      return;
+    }
+
+    setError("")
+  }
+
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+
+      <div className='flex items-center justify-center mt-28'>
+        <div className='w-96 border rounded bg-white px-7 py-10'>
+
+          <form onSubmit={handleLogin}>
+
+            <h4 className='text-2xl mb-7'>Login</h4>
+
+            <input type="text" className='input-box' placeholder='Email' value={email}
+              onChange={(e)=> setEmail(e.target.value)}
+            />
+
+
+            <InputPassword value={password} onChange={(e)=> setPassword(e.target.value)}/>
+
+            {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+
+            <button type='submit' className='btn-primary'>
+              Login
+            </button>
+
+            <p className='text-sm text-center mt-4'>
+              No account ?{""}
+              <Link to='/signup' className='font-medium text-primary underline'>
+                Create a new account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
