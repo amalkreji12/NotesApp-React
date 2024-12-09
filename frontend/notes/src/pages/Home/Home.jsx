14d049800a8449b85991b0182fc7c962f80c1739
Amal K Reji <amalreji45.amal@gmail.com>
2024-12-09 14:14:11 +0530

    profilecard added

--- frontend/notes/src/pages/Home/Home.jsx
+++ frontend/notes/src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
 
 
 function Home() {
   return (
-    <div>Dashboard</div>
+    <div>
+        <NavBar/>
+    </div>
   )
 }
 
