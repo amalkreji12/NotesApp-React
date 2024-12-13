2e7385db3a4ab0f9de0d4e10d0ce64d20e811f7a
Amal K Reji <amalreji45.amal@gmail.com>
2024-12-13 12:32:13 +0530

    signUp query changed

--- backend/controller/user-helper.js
+++ backend/controller/user-helper.js
@@ -1,11 +1,14 @@
 const User = require('../models/userModel');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
     doSignUp(userDetails, res) {
         return new Promise(async (resolve, reject) => {
             try {
+                userDetails.password = await bcrypt.hash(userDetails.password,10);
+
                 let isUserExist = await User.findOne({ email: userDetails.email });
                 if (isUserExist) {
                     return res.json({
@@ -22,6 +25,16 @@ module.exports = {
             };
         });
     },
+
+    // doLogin(userDetails) {
+    //     return new Promise(async(resolve,reject) => {
+    //        let user = await User.findOne({email:userDetails.email});
+    //        if(user){
+
+    //        }
+        
+    //     })
+    // }
 
 
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -34,6 +34,19 @@ router.post('/signup', async (req, res) => {
     });
 });
 
+router.post('/login', (req,res) => {
+    const { email, password } = req.body;
+
+    if(!email) {
+        return res.status(400).json({message: "Invalid email"});
+    }
+
+    if(!password) {
+        return res.status(400).json({message: "Invalid password"});
+    }
+    
+})
+
 
 
 
